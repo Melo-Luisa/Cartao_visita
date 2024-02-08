903a51fdbc5ebc46356a021ca8188e8ca761952a
Melo-Luisa <97964206+Melo-Luisa@users.noreply.github.com>
2024-02-08 15:25:42 -0300

    up

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ function email(){
         window.location.href = linkEmail;
     });
 }
+function copia(){
+    document.getElementById('copyButton').addEventListener('click', function() {
+        var copyText = "10756300967";
+        
+        // Seleciona o texto dentro do textarea
+        copyText.select();
+        
+        // Copia o texto para a área de transferência
+        document.execCommand('copy');
+        
+        // Deseleciona o texto
+        copyText.setSelectionRange(0, 0);
+    
+        console.log('Texto copiado com sucesso!');
+    });
+    
+}
 email();
+copia();
